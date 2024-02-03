9958fde22d7c685ceece0b062d45be409309f8e8
Antim <you@example.com>
2024-02-04 00:03:04 +0600

    components are created

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlyLo7ai_KECifCXvp55eISZ7zDkkfPNU",
+  authDomain: "simple-firebase-75a17.firebaseapp.com",
+  projectId: "simple-firebase-75a17",
+  storageBucket: "simple-firebase-75a17.appspot.com",
+  messagingSenderId: "1040555073285",
+  appId: "1:1040555073285:web:a1ca67d8b146c686a3a16c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
